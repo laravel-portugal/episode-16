ffef6762b58fac0891e870e6ac9123007303f7ee
Andre Sardo <afsardo@gmail.com>
2018-07-21 00:18:31 +0100

    Step 2 - Refactor to make default action more explicit

--- src/GildedRose.php
+++ src/GildedRose.php
@@ -31,15 +31,13 @@ class Item {
         $this->quality = $quality;
     }
 
-    public function updateQuality() 
+    public function updateQuality()
     {
-        if ($this->name != 'Aged Brie' and $this->name != 'Backstage passes to a TAFKAL80ETC concert') {
-            if ($this->quality > 0) {
-                if ($this->name != 'Sulfuras, Hand of Ragnaros') {
-                    $this->quality = $this->quality - 1;
-                }
-            }
-        } else {
+        if ($this->name != 'Sulfuras, Hand of Ragnaros') {
+            $this->sellIn--;
+        }
+
+        if ($this->name == 'Aged Brie' || $this->name == 'Backstage passes to a TAFKAL80ETC concert') {
             if ($this->quality < 50) {
                 $this->quality = $this->quality + 1;
                 if ($this->name == 'Backstage passes to a TAFKAL80ETC concert') {
@@ -55,27 +53,27 @@ class Item {
                     }
                 }
             }
-        }
-        
-        if ($this->name != 'Sulfuras, Hand of Ragnaros') {
-            $this->sellIn = $this->sellIn - 1;
-        }
-        
-        if ($this->sellIn < 0) {
-            if ($this->name != 'Aged Brie') {
-                if ($this->name != 'Backstage passes to a TAFKAL80ETC concert') {
-                    if ($this->quality > 0) {
-                        if ($this->name != 'Sulfuras, Hand of Ragnaros') {
-                            $this->quality = $this->quality - 1;
-                        }
+
+            if ($this->sellIn < 0) {
+                if ($this->name == 'Aged Brie') {
+                    if ($this->quality < 50) {
+                        $this->quality = $this->quality + 1;
                     }
+                }
+
+                if ($this->name == 'Backstage passes to a TAFKAL80ETC concert') {
+                    $this->quality = 0;
+                }
+            }
+        } else {
+            if ($this->name != 'Sulfuras, Hand of Ragnaros') {
+                if ($this->sellIn < 0) {
+                    $this->quality = $this->quality - 2;
                 } else {
-                    $this->quality = $this->quality - $this->quality;
+                    $this->quality = $this->quality - 1;
                 }
-            } else {
-                if ($this->quality < 50) {
-                    $this->quality = $this->quality + 1;
-                }
+        
+                $this->quality = max($this->quality, 0);
             }
         }
     }
